92689f3c70e5968736d75443466b1602674ffc2c
Jorge Bodega <jorge.bodega.f@gmail.com>
2021-01-08 22:35:17 +0100

    Black

--- dotpyle/main.py
+++ dotpyle/main.py
@@ -4,7 +4,7 @@ from subprocess import Popen, run
 from os import getenv, path, mkdir
 
 
-DOTPYLE_FILE = 'dotpyle.yml'
+DOTPYLE_FILE = "dotpyle.yml"
 
 
 @click.group()
@@ -12,10 +12,24 @@ def dotpyle():
     """ Needed to create different commands with different options """
     pass
 
+
 @click.command()
-@click.option('-u', '--url',required=True, help='Git url of existing repo (GitHub, GitLab, ...)')
-@click.option('-p', '--protocol', required=True, type=click.Choice(['git', 'https']),  help='Git protocol')
-@click.option('-t', '--token', required=False, help='Git token required if the Git repo is private')
+@click.option(
+    "-u", "--url", required=True, help="Git url of existing repo (GitHub, GitLab, ...)"
+)
+@click.option(
+    "-p",
+    "--protocol",
+    required=True,
+    type=click.Choice(["git", "https"]),
+    help="Git protocol",
+)
+@click.option(
+    "-t",
+    "--token",
+    required=False,
+    help="Git token required if the Git repo is private",
+)
 def init(url, protocol, token):
     """
     This command will clone an existing Git repository on ${XDG_CONFIG_HOME}/dotpyle
@@ -24,30 +38,34 @@ def init(url, protocol, token):
     """
 
     default_config_path = get_default_path()
-    print('default_config_path: ', default_config_path)
+    print("default_config_path: ", default_config_path)
 
     # Check if dotpyle config file exist
     if not path.isdir(default_config_path):
         # Create config file
-        print('Creating ', default_config_path)
+        print("Creating ", default_config_path)
         mkdir(default_config_path)
 
     # If token is defined it is mean that repo is private
     if token:
         # git clone https://<token>@github.com/owner/repo.git
-        url = url[0:8] + token + '@' + url[9:]
+        url = url[0:8] + token + "@" + url[9:]
         # Token is not need to be stored, origin url does store it for us
 
-    print ('Cloning into ', url)
+    print("Cloning into ", url)
     # Bring the remote repo
-    process = Popen(['git', 'clone', url, default_config_path], stdout=subprocess.PIPE, universal_newlines=True)
+    process = Popen(
+        ["git", "clone", url, default_config_path],
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
     while True:
         output = process.stdout.readline()
         print(output.strip())
         # Do something else
         return_code = process.poll()
         if return_code is not None:
-            print('RETURN CODE', return_code)
+            print("RETURN CODE", return_code)
             # Process has finished, read rest of the output
             for output in process.stdout.readlines():
                 print(output.strip())
@@ -66,31 +84,30 @@ def init(url, protocol, token):
 
 
 @click.command()
-@click.option('--path', help='Path file')
-def add (path):
+@click.option("--path", help="Path file")
+def add(path):
     pass
+
 
 @click.command()
-def commit ():
+def commit():
     pass
+
 
 @click.command()
-def config ():
+def config():
     pass
-
-
 
 
 def get_default_path():
     # Get defautl config path
     # If it does not exist, take .config
-    default_config_path = getenv('XDG_CONFIG_HOME', default='~/.config/')
+    default_config_path = getenv("XDG_CONFIG_HOME", default="~/.config/")
     # Expand ~ home
     default_config_path = path.expanduser(default_config_path)
 
-    default_config_path += 'dotpyle/'
+    default_config_path += "dotpyle/"
     return default_config_path
-
 
 
 # Add commands to group
@@ -99,9 +116,10 @@ dotpyle.add_command(add)
 dotpyle.add_command(commit)
 dotpyle.add_command(config)
 
+
 def main():
     dotpyle()
 
-if __name__ == '__main__':
-    dotpyle()
 
+if __name__ == "__main__":
+    dotpyle()
