585db740f2114e58d99e44d887c41027702de069
Jorge Bodega <jorge.bodega.f@gmail.com>
2021-04-23 00:44:24 +0200

    Try to add some tests into ConfigHandler constructor

--- tests/services/test_config_handler.py
+++
@@ -1,4 +0,0 @@
-from os import path
-from dotpyle.services.config_handler import ConfigHandler
-import pytest
-from subprocess import CalledProcessError, SubprocessError
--- tests/unitary/services/test_config_checker.py
+++ tests/unitary/services/test_config_checker.py
@@ -1,4 +1,3 @@
-from os import path
 import pytest
 from tests.utils.mocks.config_valid_cases import valid_cases
 from tests.utils.mocks.config_invalid_cases import invalid_cases
@@ -6,7 +5,7 @@ from dotpyle.services.config_checker import ConfigChecker
 
 
 @pytest.mark.parametrize("config", [*valid_cases])
-def test_check_config_valid_configs(config):
+def test_config_checker_valid_configs(config):
     checker = ConfigChecker()
 
     errors = checker.check_config(config)
@@ -14,7 +13,7 @@ def test_check_config_valid_configs(config):
 
 
 @pytest.mark.parametrize("config", [*invalid_cases])
-def test_check_config_invalid_configs(config):
+def test_config_checker_invalid_configs(config):
     checker = ConfigChecker()
 
     errors = checker.check_config(config)
